9ae05004a49277d04a8b5186bf253d88cadbc7eb
afrayz29 <alfrayman@gmail.com>
2020-01-21 18:20:56 +0000

    css for side Nav

--- src/js/component/sidebar.js
+++ src/js/component/sidebar.js
@@ -14,8 +14,11 @@ export const Sidebar = () => {
 			<div id="mySidenav" className={sideNav}>
 				<a href="javascript:void(0)" className="closebtn" onClick={() => triggerSideBar()} />
 				<Link to="/signup/">sign up</Link>
-                <Link to="/signup/">Pet Profile</Link>
-                <Link to="/signup/">Settings</Link>
+				<Link to="/signup/">Pet Profile</Link>
+				<Link to="/signup/">Settings</Link>
+				<Link to="/signup/">contact us</Link>
+				<Link to="/signup/">order merchandise</Link>
+				<Link to="/signup/">book services</Link>
 			</div>
 			<span style={{ fontSize: "30px", cursor: "pointer" }} onClick={() => triggerSideBar()}>
 				☰{" "}
