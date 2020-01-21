c1bb5994247b8768bcebbabebcc12779f3693d69
afrayz29 <alfrayman@gmail.com>
2020-01-21 17:09:03 +0000

    receive update

--- src/js/component/sidebar.js
+++ src/js/component/sidebar.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
-	const [sideNav, setSideNav] = useState("open");
+	const [sideNav, setSideNav] = useState("close");
 	const triggerSideBar = () => {
 		if (sideNav == "open") {
 			setSideNav("close");
@@ -15,8 +15,9 @@ export const Sidebar = () => {
 				<a href="javascript:void(0)" className="closebtn" onClick={() => triggerSideBar()} />
 				<a href="#">About</a>
 				<a href="#">Services</a>
-				<a href="#">Clients</a>
+				<a href="#">Partners</a>
 				<a href="#">Contact</a>
+                <a href="#">Sign Up</a>
 			</div>
 			<span style={{ fontSize: "30px", cursor: "pointer" }} onClick={() => triggerSideBar()}>
 				☰{" "}
