5d47c3776d4803076e17f870e64bbb45d4c73bc9
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-01-21 03:27:57 +0000

    add camera code

--- src/js/views/camera.js
+++ src/js/views/camera.js
@@ -1,23 +1,70 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
-import { Context } from "../store/appContext";
+import Webcam from "react-webcam";
+// import { Context } from "../store/appContext";
 
 import "../../styles/demo.scss";
 
 export const Camera = () => {
-	const { store, actions } = useContext(Context);
+	// const { store, actions } = useContext(Context);
 
-	return (
-		<>
-			<div className="emptyDiv" />
-			<div className="container">
-				<h1> Camera </h1>
-				<br />
-				<Link to="/">
-					<button className="btn btn-primary">Back home</button>
-				</Link>
-			</div>
-		</>
-	);
+	const videoConstraints = {
+		width: 1280,
+		height: 720,
+		facingMode: "user"
+	};
+
+	const WebcamCapture = () => {
+		const webcamRef = React.useRef(null);
+
+		const capture = React.useCallback(
+			() => {
+				const imageSrc = webcamRef.current.getScreenshot();
+			},
+			[webcamRef]
+		);
+
+		class WebcamCapture extends React.Component {
+			render() {
+				const videoConstraints = {
+					facingMode: "user"
+				};
+
+				return <Webcam videoConstraints={videoConstraints} />;
+			}
+		}
+
+		class WebcamCaptured extends React.Component {
+			render() {
+				const videoConstraints = {
+					facingMode: { exact: "environment" }
+				};
+
+				return <Webcam videoConstraints={videoConstraints} />;
+			}
+		}
+		return (
+			<>
+				<div className="emptyDiv" />
+				<div className="container">
+					<h1> Camera </h1>
+					<div className="camera">
+						<Webcam
+							audio={false}
+							height={720}
+							ref={webcamRef}
+							screenshotFormat="image/jpeg"
+							width={1280}
+							videoConstraints={videoConstraints}
+						/>
+						<button onClick={capture}>Capture photo</button>
+					</div>
+					<br />
+					<Link to="/">
+						<button className="btn btn-primary">Back home</button>
+					</Link>
+				</div>
+			</>
+		);
+	};
 };
