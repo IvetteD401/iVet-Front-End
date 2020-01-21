d316c0918637b94413839a3cd3c478b4010eca68
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-01-21 20:20:21 +0000

    edits to links to sidenav

--- src/js/component/sidebar.js
+++ src/js/component/sidebar.js
@@ -15,10 +15,10 @@ export const Sidebar = () => {
 				<a href="javascript:void(0)" className="closebtn" onClick={() => triggerSideBar()} />
 				<Link to="/signup/">sign up</Link>
 				<Link to="/signup/">Pet Profile</Link>
-				<Link to="/signup/">Settings</Link>
-				<Link to="/signup/">contact us</Link>
-				<Link to="/signup/">order merchandise</Link>
-				<Link to="/signup/">book services</Link>
+				<Link to="/settings/">Settings</Link>
+				<Link to="/contactUs/">contact us</Link>
+				<Link to="/orderform/">order merchandise</Link>
+				<Link to="/bookservices/">book services</Link>
 			</div>
 			<span style={{ fontSize: "30px", cursor: "pointer" }} onClick={() => triggerSideBar()}>
 				☰{" "}
