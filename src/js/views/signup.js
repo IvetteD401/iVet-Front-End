06183e8e857b4236b37789360de9994e777cbbd0
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-01-21 18:38:17 +0000

    sign up form html inserted

--- src/js/views/signup.js
+++ src/js/views/signup.js
@@ -13,45 +13,48 @@ export const Signup = () => {
 			<div className="emptyDiv" />
 			<div className="container">
 				Sign up
-				<form className="form-signin">
-					<img
-						className="mb-4"
-						src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg"
-						alt=""
-						width={72}
-						height={72}
-					/>
-					<h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-					<label htmlFor="inputEmail" className="sr-only">
-						Email address
-					</label>
-					<input
-						type="email"
-						id="inputEmail"
-						className="form-control"
-						placeholder="Email address"
-						required
-						autoFocus
-					/>
-					<label htmlFor="inputPassword" className="sr-only">
-						Password
-					</label>
-					<input
-						type="password"
-						id="inputPassword"
-						className="form-control"
-						placeholder="Password"
-						required
-					/>
-					<div className="checkbox mb-3">
-						<label>
-							<input type="checkbox" defaultValue="remember-me" /> Remember me
+				<form action="/action_page.php" style={{ border: "1px solid #ccc" }}>
+					<div className="container">
+						<h1>Sign Up</h1>
+						<p>Please fill in this form to create an account.</p>
+						<hr />
+						<label htmlFor="email">
+							<b>Email</b>
 						</label>
+						<input type="text" placeholder="Enter Email" name="email" required />
+						<label htmlFor="psw">
+							<b>Password</b>
+						</label>
+						<input type="password" placeholder="Enter Password" name="psw" required />
+						<label htmlFor="psw-repeat">
+							<b>Repeat Password</b>
+						</label>
+						<input type="password" placeholder="Repeat Password" name="psw-repeat" required />
+						<label>
+							<input
+								type="checkbox"
+								defaultChecked="checked"
+								name="remember"
+								style={{ marginBottom: "15px" }}
+							/>{" "}
+							Remember me
+						</label>
+						<p>
+							By creating an account you agree to our{" "}
+							<a href="#" style={{ color: "dodgerblue" }}>
+								Terms &amp; Privacy
+							</a>
+							.
+						</p>
+						<div className="clearfix">
+							<button type="button" className="cancelbtn">
+								Cancel
+							</button>
+							<button type="submit" className="signupbtn">
+								Sign Up
+							</button>
+						</div>
 					</div>
-					<button className="btn btn-lg btn-primary btn-block" type="submit">
-						Sign in
-					</button>
-					<p className="mt-5 mb-3 text-muted">© 2017-2018</p>
 				</form>
 				<br />
 				<Link to="/">
