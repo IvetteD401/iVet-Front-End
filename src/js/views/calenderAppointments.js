5d73e3b432e3b87a73fed361849d0e5b10157fdb
afrayz29 <alfrayman@gmail.com>
2020-01-23 20:12:48 +0000

    hfefdk

--- src/js/views/calenderAppointments.js
+++ src/js/views/calenderAppointments.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
+import React, { Component } from "react";
 import { Context } from "../store/appContext";
 
 import "../../styles/demo.scss";
@@ -10,13 +10,16 @@ export const CalenderAppointments = () => {
 
 	return (
 		<>
-			<div className="container">
-				<h1> CalenderAppointments </h1>
-				<br />
-				<Link to="/">
-					<button className="btn btn-primary">Back home</button>
-				</Link>
-			</div>
+			{
+				<div className="container">
+					<h1> CalenderAppointments </h1>
+					<br />
+					<Link to="/">
+						<button className="btn btn-primary">Back home</button>
+					</Link>
+				</div>
+			}
+			}
 		</>
 	);
 };
