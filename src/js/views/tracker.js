64e3f3e33c27a82875f5f4d66e456e63429f3351
Jose Padron <josepadron370@gmail.com>
2020-01-30 15:52:35 +0000

    Fixing texts in cards

--- src/js/views/tracker.js
+++ src/js/views/tracker.js
@@ -128,10 +128,7 @@ export const Tracker = () => {
 									</text>
 								</svg>
 								<div className="card-body">
-									<p className="card-text">
-										This is a wider card with supporting text below as a natural lead-in to
-										additional content. This content is a little bit longer.
-									</p>
+									<p className="card-text">The blood pressure measurements will be displayed here.</p>
 									<div className="d-flex justify-content-between align-items-center">
 										<div className="btn-group">
 											<button type="button" className="btn btn-sm btn-outline-secondary">
@@ -200,10 +197,7 @@ export const Tracker = () => {
 									</text>
 								</svg>
 								<div className="card-body">
-									<p className="card-text">
-										This is a wider card with supporting text below as a natural lead-in to
-										additional content. This content is a little bit longer.
-									</p>
+									<p className="card-text">The location of your pet will be displayed here!</p>
 									<div className="d-flex justify-content-between align-items-center">
 										<div className="btn-group">
 											<button type="button" className="btn btn-sm btn-outline-secondary">
