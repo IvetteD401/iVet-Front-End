347e57b6e621f43e300914f77896416feca10fb6
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-02-09 18:32:17 +0000

    home presentation view styled

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 
 export const Home = () => (
 	<>
-		<section className="jumbotron lead text-center">
+		<section className="jumbotron lead leadLogo mx-auto text-center">
 			<div className="container">
+				<br />
+
 				<h1>
-					<strong>iVet</strong>{" "}
+					<strong>i-Vet</strong>{" "}
 				</h1>
 				<h3>
 					{'"'}Your one stop shop for all of your dogs health needs!{'"'}
@@ -15,8 +17,8 @@ export const Home = () => (
 			</div>
 		</section>
 
-		<section className="jumbotron lead text-center mx-auto">
-			<div className="card" style={{ maxWidth: "100%" }}>
+		<section className="jumbotron homeJumbo lead text-center mx-auto">
+			<div className="card about" style={{ maxWidth: "100%" }}>
 				<div className="row no-gutters">
 					<div className="col-md-4">
 						<img
@@ -27,30 +29,43 @@ export const Home = () => (
 					</div>
 					<div className="col-md-8">
 						<div className="card-body">
-							<h5 className="card-title">Card title</h5>
-							<p className="card-text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
-							</p>
-							<p className="card-text">
-								<small className="text-muted">Last updated 3 mins ago</small>
-							</p>
+							<h2 className="card-title">
+								<strong>Ivette Dominguez</strong>
+							</h2>
+							<h4>
+								<p className="card-text">
+									<strong>
+										Motivated and resourceful individual capable of providing the highest quality of
+										support with excellent organizational skills that allow to work productively in
+										a fast-paced environment without direct supervision.
+									</strong>
+								</p>
+								<p className="card-text">
+									<small className="text-muted">Last updated 3 mins ago</small>
+								</p>
+							</h4>
 						</div>
 					</div>
 				</div>
 			</div>
 		</section>
-		<div className="emptyDiv" />
-		<section className="jumbotron lead text-center">
-			<div className="card" style={{ maxWidth: "100%" }}>
+
+		<section className="jumbotron homeJumbo lead text-center">
+			<div className="card about" style={{ maxWidth: "100%" }}>
 				<div className="row no-gutters">
 					<div className="col-md-8">
 						<div className="card-body">
-							<h5 className="card-title">Card title</h5>
-							<p className="card-text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
-							</p>
+							<h2 className="card-title">
+								<strong>Antuan Freyman</strong>
+							</h2>
+							<h4>
+								<strong>
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural lead-in to
+										additional content. This content is a little bit longer.
+									</p>
+								</strong>
+							</h4>
 							<p className="card-text">
 								<small className="text-muted">Last updated 3 mins ago</small>
 							</p>
@@ -66,9 +81,9 @@ export const Home = () => (
 				</div>
 			</div>
 		</section>
-		<div className="emptyDiv" />
-		<section className="jumbotron lead text-center">
-			<div className="card" style={{ maxWidth: "100%" }}>
+
+		<section className="jumbotron homeJumbo lead text-center">
+			<div className="card about" style={{ maxWidth: "100%" }}>
 				<div className="row no-gutters">
 					<div className="col-md-4">
 						<img
@@ -79,11 +94,17 @@ export const Home = () => (
 					</div>
 					<div className="col-md-8">
 						<div className="card-body">
-							<h5 className="card-title">Card title</h5>
-							<p className="card-text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
-							</p>
+							<h2 className="card-title">
+								<strong>Jose Padron</strong>
+							</h2>
+							<h4>
+								<strong>
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural lead-in to
+										additional content. This content is a little bit longer.
+									</p>
+								</strong>
+							</h4>
 							<p className="card-text">
 								<small className="text-muted">Last updated 3 mins ago</small>
 							</p>
@@ -92,14 +113,14 @@ export const Home = () => (
 				</div>
 			</div>
 		</section>
-		<div className="emptyDiv" />
-		<section className="jumbotron lead text-center"></section>
-		<div className="emptyDiv" />
-		<div className="emptyDiv" />
-		<section className="jumbotron lead text-center">
+
+		<section className="jumbotron lead leadLogo startLink mx-auto text-center">
+			<br />
+			{/* <br /> */}
 			<Link to="/ivethome/">
-				<button className="btn lg btn-dark my-2">Lets Start Here</button>
+				<button className="btn lg btn-success">Start Here</button>
 			</Link>
 		</section>
+		<i className="fa fa-long-arrow-up float-right"></i>
 	</>
 );
