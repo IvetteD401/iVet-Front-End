02c1a5f653e008f4c61012b8d0fade743e52cbe0
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-02-12 21:32:12 +0000

    add caregiver

--- src/js/views/add_caregiver.js
+++ src/js/views/add_caregiver.js
@@ -19,71 +19,30 @@ export const AddCaregiver = () => {
 					<div className="Padding-for-Div">
 						<form>
 							<div className="form-group">
-								<label>Pet{"'"}s Name</label>
+								<label>caregiver{"'"}s Name</label>
 								<input
 									type="text"
 									className="form-control"
-									placeholder="Pet Name"
-									onChange={e => setPetname(e.target.value)}
-								/>
-							</div>
-							<div className="form-group">
-								<label>Vet{"'"}s Name</label>
-								<input
-									type="text"
-									className="form-control"
-									placeholder="Vet Name"
-									onChange={e => setVetname(e.target.value)}
-								/>
-							</div>
-							<div className="form-group">
-								<label>Groomer{"'"}s Name</label>
-								<input
-									type="text"
-									className="form-control"
-									placeholder="Vet Name"
-									onChange={e => setGroomername(e.target.value)}
-								/>
-							</div>
-
-							<div className="form-group">
-								<label>Care Giver</label>
-								<input
-									type="phone"
-									className="form-control"
-									placeholder="Enter care givers here"
+									placeholder="Caregivers Name"
 									onChange={e => setCaregiver(e.target.value)}
 								/>
 							</div>
 							<div className="form-group">
-								<label>Likes</label>
+								<label>Caregiver{"'"}s Email</label>
 								<input
 									type="text"
 									className="form-control"
-									placeholder="Enter likes"
-									onChange={e => setLikes(e.target.value)}
+									placeholder="email"
+									onChange={e => setemail(e.target.value)}
 								/>
 							</div>
-							<Link to={"/petProfile"}>
-								<button
-									type="button"
-									className="btn btn-primary form-control"
-									onClick={() => {
-										actions.addPet_Profile(
-											vetname,
-											groomername,
-											// vet_address,
-											// groomer_address,
-											// insurance_policy,
-											// insurance_provider,
-											petname,
-											caregiver,
-											likes
-										);
-									}}>
-									save
-								</button>
-							</Link>
+							<button
+								type="button"
+								className="btn btn-lg btn-dark btn-block"
+								data-toggle="modal"
+								data-target="#exampleModal">
+								Book!
+							</button>
 							<Link className="mt-3 w-100 text-center" to="/">
 								or get back to contacts
 							</Link>
