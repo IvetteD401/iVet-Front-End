3c20efb04bd697b619e4e2fa78923150c577c881
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-02-13 05:01:33 +0000

    fixed antuan card on home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -49,32 +49,22 @@ export const Home = () => (
 				</div>
 			</div>
 		</section>
-<<<<<<< HEAD
-        
-                    <section className="jumbotron homeJumbo resize lead text-center mx-auto">
-=======
 		<section className="jumbotron homeJumbo resize lead text-center mx-auto">
->>>>>>> ce7abb584444317fcc668276cb57506f243461a8
 			<div className="card about resize mx-auto" style={{ maxWidth: "100%" }}>
 				<div className="row no-gutters">
-					<div className="col-md-4">
-						<img
-							src="https://avatars3.githubusercontent.com/u/57875480?s=460&v=4"
-							className="card-img"
-							alt="..."
-						/>
-					</div>
 					<div className="col-md-8">
 						<div className="card-body">
 							<h2 className="card-title">
-								<strong>Ivette Dominguez</strong>
+								<strong>Antuan Freyman</strong>
 							</h2>
 							<h4>
 								<p className="card-text">
 									<strong>Full Stack Web Developer</strong>
-									<br /> Project Manager - Lead Project Developer
+									<br />
 								</p>
 							</h4>
+							<br />
+							<br />
 							<h6>
 								<p>
 									{" "}
@@ -84,12 +74,6 @@ export const Home = () => (
 							</h6>
 						</div>
 					</div>
-				</div>
-			</div>
-		</section>
-        <section className="jumbotron homeJumbo resize lead text-center mx-auto">
-			<div className="card about resize mx-auto" style={{ maxWidth: "100%" }}>
-				<div className="row no-gutters">
 					<div className="col-md-4">
 						<img
 							src="https://avatars1.githubusercontent.com/u/57456107?s=400&u=4fe460aaaa3bca0965c58477bf313979d30a8555&v=4"
@@ -97,14 +81,9 @@ export const Home = () => (
 							alt="..."
 						/>
 					</div>
-                    </div>
-                    </div>
-                    </section>
-                  
-		
-					
-						
-		
+				</div>
+			</div>
+		</section>
 
 		<section className="jumbotron lead leadLogo startLink mx-auto text-center">
 			<br />
