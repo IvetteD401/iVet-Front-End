659b08de71de32d7976f2a6a1d49e7433a1b1f15
afrayz29 <alfrayman@gmail.com>
2020-02-13 02:13:12 +0000

    doguments

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -49,34 +49,43 @@ export const Home = () => (
 				</div>
 			</div>
 		</section>
-
-		<section className="jumbotron homeJumbo resize lead text-center mx-auto">
+        
+                    <section className="jumbotron homeJumbo resize lead text-center mx-auto">
 			<div className="card about resize mx-auto" style={{ maxWidth: "100%" }}>
 				<div className="row no-gutters">
+					<div className="col-md-4">
+						<img
+							src="https://avatars3.githubusercontent.com/u/57875480?s=460&v=4"
+							className="card-img"
+							alt="..."
+						/>
+					</div>
 					<div className="col-md-8">
 						<div className="card-body">
 							<h2 className="card-title">
-								<strong>Antuan Frayman</strong>
+								<strong>Ivette Dominguez</strong>
 							</h2>
 							<h4>
-								<strong>
-									<p className="card-text">
-										<strong>Full Stack Web Developer</strong>
-										<br /> Project Developer - 2nd Lead Project Developer
-										<br />
-									</p>
-								</strong>
+								<p className="card-text">
+									<strong>Full Stack Web Developer</strong>
+									<br /> Project Manager - Lead Project Developer
+								</p>
+							</h4>
+							<h6>
 								<p>
 									{" "}
 									Technologies used: Python, Raspberry Pi, MySQL,
 									<br /> JavaScript, ReactJS, Bootstrap
 								</p>
-							</h4>
-							<p className="card-text">
-								<small className="text-muted">Last updated 3 mins ago</small>
-							</p>
+							</h6>
 						</div>
 					</div>
+				</div>
+			</div>
+		</section>
+        <section className="jumbotron homeJumbo resize lead text-center mx-auto">
+			<div className="card about resize mx-auto" style={{ maxWidth: "100%" }}>
+				<div className="row no-gutters">
 					<div className="col-md-4">
 						<img
 							src="https://avatars1.githubusercontent.com/u/57456107?s=400&u=4fe460aaaa3bca0965c58477bf313979d30a8555&v=4"
@@ -84,9 +93,14 @@ export const Home = () => (
 							alt="..."
 						/>
 					</div>
-				</div>
-			</div>
-		</section>
+                    </div>
+                    </div>
+                    </section>
+                  
+		
+					
+						
+		
 
 		<section className="jumbotron lead leadLogo startLink mx-auto text-center">
 			<br />
